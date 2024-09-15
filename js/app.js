6ecf1b2173f47b3a854ceefed6ce1977dd1f6652
gonzagordo <agustingonzaga77@gmail.com>
2024-09-15 11:36:04 +0200

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,4 @@
+document.getElementById('myButton').addEventListener('click', function() {
+    this.style.display = 'none';  // Oculta el botón
+    document.getElementById('message').style.display = 'block';  // Muestra el mensaje
+});
